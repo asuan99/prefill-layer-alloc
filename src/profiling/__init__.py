e1da0399ba871a94be1e16b49723ac2816731c00
asuan99 <65912229+asuan99@users.noreply.github.com>
2026-04-26 17:37:54 +0900

    Add prefill layer sweep scripts and update profiling module exports

--- src/profiling/__init__.py
+++ src/profiling/__init__.py
@@ -1,4 +1,20 @@
 from .nvml_monitor import NVMLMonitor
 from .metrics import LatencyMeter, BandwidthEstimator
+from .nvtx_markers import NVTXMarker
+from .wave_estimator import WaveEstimator, WaveStats, compute_wave_stats
+from .ncu_runner import NCURunner, NCU_METRICS_WAVE, NCU_METRICS_MEMORY, NCU_METRICS_COMPUTE, NCU_METRICS_FULL
 
-__all__ = ["NVMLMonitor", "LatencyMeter", "BandwidthEstimator"]
+__all__ = [
+    "NVMLMonitor",
+    "LatencyMeter",
+    "BandwidthEstimator",
+    "NVTXMarker",
+    "WaveEstimator",
+    "WaveStats",
+    "compute_wave_stats",
+    "NCURunner",
+    "NCU_METRICS_WAVE",
+    "NCU_METRICS_MEMORY",
+    "NCU_METRICS_COMPUTE",
+    "NCU_METRICS_FULL",
+]
